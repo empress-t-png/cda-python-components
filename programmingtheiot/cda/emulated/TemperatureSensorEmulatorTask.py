e0507e0579316ab7e1394ac499eb895413b0fc4a
Tosin <tosin.e@northeastern.edu>
2025-10-12 23:18:15 -0400

    Completed PIOT-CDA-04-001: Created all sensor emulator modules

--- programmingtheiot/cda/emulated/TemperatureSensorEmulatorTask.py
+++ programmingtheiot/cda/emulated/TemperatureSensorEmulatorTask.py
@@ -5,29 +5,40 @@
 # found in the LICENSE file at the top level of this repository.
 # 
 
-from programmingtheiot.data.SensorData import SensorData
+import logging
+
 import programmingtheiot.common.ConfigConst as ConfigConst
+
+from programmingtheiot.common.ConfigUtil import ConfigUtil
 from programmingtheiot.cda.sim.BaseSensorSimTask import BaseSensorSimTask
+from programmingtheiot.data.SensorData import SensorData
+
+from pisense import SenseHAT
+
 class TemperatureSensorEmulatorTask(BaseSensorSimTask):
 	"""
 	Temperature sensor emulator that generates simulated temperature data.
 	"""
 
-	def __init__(self, dataSet = None):
+	def __init__(self, dataSet=None):
 		super(TemperatureSensorEmulatorTask, self).__init__(
 			name=ConfigConst.TEMP_SENSOR_NAME,
 			typeID=ConfigConst.TEMP_SENSOR_TYPE,
-			dataSet=dataSet
+			dataSet=dataSet,
+			minVal=0.0,
+			maxVal=100.0
 		)
+		
+		enableEmulation = ConfigUtil().getBoolean(
+			ConfigConst.CONSTRAINED_DEVICE, ConfigConst.ENABLE_EMULATOR_KEY)
+		
+		self.sh = SenseHAT(emulate=enableEmulation)
 	
 	def generateTelemetry(self) -> SensorData:
-		sensorData = SensorData(
-			typeID=ConfigConst.TEMP_SENSOR_TYPE,
-			name=ConfigConst.TEMP_SENSOR_NAME
-		)
-		
-		sensorVal = self.generator.generateTelemetry()
+		sensorData = SensorData(name=self.getName(), typeID=self.getTypeID())
+		sensorVal = self.sh.environ.temperature
 		
 		sensorData.setValue(sensorVal)
+		self.latestSensorData = sensorData
 		
 		return sensorData
