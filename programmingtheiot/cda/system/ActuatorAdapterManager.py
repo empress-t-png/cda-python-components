d891e885e61499161e2ad76e9e4fa9188f25d23c
Tosin <tosin.e@northeastern.edu>
2025-10-13 20:30:04 -0400

    Completed PIOT-CDA-04-004: Added emulator functionality to ActuatorAdapterManager

--- programmingtheiot/cda/system/ActuatorAdapterManager.py
+++ programmingtheiot/cda/system/ActuatorAdapterManager.py
@@ -7,103 +7,97 @@
 
 import logging
 
+from importlib import import_module
+
 import programmingtheiot.common.ConfigConst as ConfigConst
 
 from programmingtheiot.common.ConfigUtil import ConfigUtil
-from programmingtheiot.common.IDataMessageListener import IDataMessageListener
-
 from programmingtheiot.data.ActuatorData import ActuatorData
-
-from programmingtheiot.cda.sim.HvacActuatorSimTask import HvacActuatorSimTask
 from programmingtheiot.cda.sim.HumidifierActuatorSimTask import HumidifierActuatorSimTask
+from programmingtheiot.cda.sim.HvacActuatorSimTask import HvacActuatorSimTask
 
 class ActuatorAdapterManager(object):
-    """
-    Manager for actuator adapter tasks.
-    """
-    
-    def __init__(self, dataMsgListener: IDataMessageListener = None):
-        self.dataMsgListener = dataMsgListener
-        
-        self.configUtil = ConfigUtil()
-        
-        self.useSimulator = \
-            self.configUtil.getBoolean(
-                section=ConfigConst.CONSTRAINED_DEVICE, 
-                key=ConfigConst.ENABLE_SIMULATOR_KEY)
-        
-        self.useEmulator = \
-            self.configUtil.getBoolean(
-                section=ConfigConst.CONSTRAINED_DEVICE, 
-                key=ConfigConst.ENABLE_EMULATOR_KEY)
-        
-        self.deviceID = \
-            self.configUtil.getProperty(
-                section=ConfigConst.CONSTRAINED_DEVICE, 
-                key=ConfigConst.DEVICE_LOCATION_ID_KEY, 
-                defaultVal=ConfigConst.NOT_SET)
-        
-        self.locationID = \
-            self.configUtil.getProperty(
-                section=ConfigConst.CONSTRAINED_DEVICE, 
-                key=ConfigConst.DEVICE_LOCATION_ID_KEY, 
-                defaultVal=ConfigConst.NOT_SET)
-        
-        self.humidifierActuator = None
-        self.hvacActuator = None
-        self.ledDisplayActuator = None
-        
-        if self.useEmulator:
-            logging.info("ActuatorAdapterManager will use EMULATORS.")
-        else:
-            logging.info("ActuatorAdapterManager will use SIMULATORS.")
-        
-        self._initEnvironmentalActuationTasks()
-    
-    def _initEnvironmentalActuationTasks(self):
-        """
-        Initialize environmental actuation tasks.
-        """
-        if not self.useEmulator:
-            logging.info("Creating actuator simulator tasks...")
-            
-            self.humidifierActuator = HumidifierActuatorSimTask()
-            self.hvacActuator = HvacActuatorSimTask()
-    
-    def sendActuatorCommand(self, data: ActuatorData) -> ActuatorData:
-        """
-        Send actuator command to the appropriate actuator.
-        """
-        if data and not data.isResponseFlagEnabled():
-            # Check if actuation event is for this device
-            if data.getLocationID() == self.locationID:
-                logging.info("Actuator command received for location ID %s. Processing...", str(data.getLocationID()))
-                
-                aType = data.getTypeID()
-                responseData = None
-                
-                if aType == ConfigConst.HUMIDIFIER_ACTUATOR_TYPE and self.humidifierActuator:
-                    responseData = self.humidifierActuator.updateActuator(data)
-                elif aType == ConfigConst.HVAC_ACTUATOR_TYPE and self.hvacActuator:
-                    responseData = self.hvacActuator.updateActuator(data)
-                elif aType == ConfigConst.LED_DISPLAY_ACTUATOR_TYPE and self.ledDisplayActuator:
-                    responseData = self.ledDisplayActuator.updateActuator(data)
-                else:
-                    logging.warning("No valid actuator type. Ignoring actuation for type: %s", data.getTypeID())
-                
-                return responseData
-            else:
-                logging.warning("Location ID doesn't match. Ignoring actuation: (me) %s != (you) %s", str(self.locationID), str(data.getLocationID()))
-        else:
-            logging.warning("Actuator request received. Message is empty or response. Ignoring.")
-        
-        return None
-    
-    def setDataMessageListener(self, listener: IDataMessageListener) -> bool:
-        """
-        Set the data message listener.
-        """
-        if listener:
-            self.dataMsgListener = listener
-            return True
-        return False
+	"""
+	Manager for actuator adapter tasks.
+	"""
+	
+	def __init__(self):
+		self.configUtil = ConfigUtil()
+		
+		self.useEmulator = \
+			self.configUtil.getBoolean(
+				section=ConfigConst.CONSTRAINED_DEVICE,
+				key=ConfigConst.ENABLE_EMULATOR_KEY)
+		
+		self.dataMsgListener = None
+		self.humidifierActuator = None
+		self.hvacActuator = None
+		self.ledDisplayActuator = None
+		
+		if self.useEmulator:
+			logging.info("ActuatorAdapterManager will use EMULATORS.")
+		else:
+			logging.info("ActuatorAdapterManager will use SIMULATORS.")
+		
+		self._initActuatorTasks()
+	
+	def _initActuatorTasks(self):
+		"""
+		Initialize actuator tasks.
+		"""
+		if not self.useEmulator:
+			logging.info("Creating actuator simulator tasks...")
+			
+			self.humidifierActuator = HumidifierActuatorSimTask()
+			self.hvacActuator = HvacActuatorSimTask()
+		else:
+			logging.info("Creating actuator emulator tasks...")
+			
+			# Load humidifier actuator emulator
+			humidModule = import_module('programmingtheiot.cda.emulated.HumidifierEmulatorTask', 'HumidifierEmulatorTask')
+			humidClazz = getattr(humidModule, 'HumidifierEmulatorTask')
+			self.humidifierActuator = humidClazz()
+			
+			# Load HVAC actuator emulator
+			hvacModule = import_module('programmingtheiot.cda.emulated.HvacEmulatorTask', 'HvacEmulatorTask')
+			hvacClazz = getattr(hvacModule, 'HvacEmulatorTask')
+			self.hvacActuator = hvacClazz()
+			
+			# Load LED display actuator emulator
+			ledModule = import_module('programmingtheiot.cda.emulated.LedDisplayEmulatorTask', 'LedDisplayEmulatorTask')
+			ledClazz = getattr(ledModule, 'LedDisplayEmulatorTask')
+			self.ledDisplayActuator = ledClazz()
+	
+	def sendActuatorCommand(self, data: ActuatorData) -> bool:
+		"""
+		Send actuator command.
+		"""
+		if data:
+			logging.info("Actuator command received for location ID %s. Processing...", data.getLocationID())
+			
+			actuatorType = data.getTypeID()
+			
+			if actuatorType == ConfigConst.HUMIDIFIER_ACTUATOR_TYPE and self.humidifierActuator:
+				self.humidifierActuator.updateActuator(data)
+				return True
+			elif actuatorType == ConfigConst.HVAC_ACTUATOR_TYPE and self.hvacActuator:
+				self.hvacActuator.updateActuator(data)
+				return True
+			elif actuatorType == ConfigConst.LED_DISPLAY_ACTUATOR_TYPE and self.ledDisplayActuator:
+				self.ledDisplayActuator.updateActuator(data)
+				return True
+			else:
+				logging.warning("No actuator of type %s available.", actuatorType)
+				return False
+		else:
+			logging.warning("Actuator data is None. Ignoring.")
+			return False
+	
+	def setDataMessageListener(self, listener) -> bool:
+		"""
+		Set the data message listener.
+		"""
+		if listener:
+			self.dataMsgListener = listener
+			return True
+		return False
